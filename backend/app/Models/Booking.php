863dd26fd7772d0ec8f415d2f85b364fd98ef351
kjICE <karl.jones@ice-comms.co.uk>
2022-02-28 22:54:48 +0000

    return casted data types

--- backend/app/Models/Booking.php
+++ backend/app/Models/Booking.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-
+    protected $casts = [
+        "students" => "array",
+        "certificate_needed" => "boolean"
+    ];
 }
