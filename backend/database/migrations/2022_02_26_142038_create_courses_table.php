01c29dab23165d5771f1251a4207eae30ba1f2ce
kjICE <karl.jones@ice-comms.co.uk>
2022-02-28 20:16:43 +0000

    add courses migraton

---
+++ backend/database/migrations/2022_02_26_142038_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $topics = ["Frontend", "Backend", "Fullstack", "Cloud", "Security"];
+
+            $table->id();
+            $table->string('name');
+            $table->enum('topic', $topics);
+            $table->tinyInteger('level');
+            $table->tinyInteger('duration');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
