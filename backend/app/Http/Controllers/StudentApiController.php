5ff7f9d45bc6150391c9d804434d3af1de62858c
kjICE <karl.jones@ice-comms.co.uk>
2022-02-28 22:18:05 +0000

    return all Students

--- backend/app/Http/Controllers/StudentApiController.php
+++ backend/app/Http/Controllers/StudentApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentApiController extends Controller
 {
@@ -13,7 +14,10 @@ class StudentApiController extends Controller
      */
     public function index()
     {
-        //
+        $code = 200;
+        $output = Student::all();
+
+        return response($output, $code);
     }
 
     /**
