38499ddc002bd388319ebb28662bdb8fa0943aff
kjICE <karl.jones@ice-comms.co.uk>
2022-02-28 23:01:33 +0000

    add fillable Booking fields

--- backend/app/Models/Booking.php
+++ backend/app/Models/Booking.php
@@ -10,4 +10,15 @@ class Booking extends Model
         "students" => "array",
         "certificate_needed" => "boolean"
     ];
+
+    protected $fillable = [
+        "course_id",
+        "location_id",
+        "trainer_id",
+        "students",
+        "start",
+        "end",
+        "certificate_needed",
+        "comments",
+    ];
 }
