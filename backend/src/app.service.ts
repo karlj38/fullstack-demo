c2a5948167a553e3a514e14ea2fec7d461733fb5
kjICE <karl.jones@ice-comms.co.uk>
2022-02-28 19:53:31 +0000

    remove nest backend

--- backend/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    // TODO: Chaneg to whichever message you feel appropriate
-    return 'Hello World!';
-  }
-}
