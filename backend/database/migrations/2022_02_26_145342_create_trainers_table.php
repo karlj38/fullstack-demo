25c4c8d7e92a2b02d21da5eca5817981f1353757
kjICE <karl.jones@ice-comms.co.uk>
2022-02-28 20:18:26 +0000

    add trainers migraton

---
+++ backend/database/migrations/2022_02_26_145342_create_trainers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->tinyInteger('level');
+            $table->json('competencies');
+            $table->boolean('needWheelchair');
+            $table->string('city');
+            $table->string('country');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainers');
+    }
+}
