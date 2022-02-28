1a1f154c67f988973ec6403e87ec544d61304ac2
kjICE <karl.jones@ice-comms.co.uk>
2022-02-28 20:19:19 +0000

    add bookings migraton

---
+++ backend/database/migrations/2022_02_26_150554_create_bookings_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained();
+            $table->foreignId('location_id')->constrained();
+            $table->foreignId('trainer_id')->constrained();
+            $table->json('students');
+            $table->date('start');
+            $table->date('end');
+            $table->boolean('certificate_needed');
+            $table->text('comments')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
