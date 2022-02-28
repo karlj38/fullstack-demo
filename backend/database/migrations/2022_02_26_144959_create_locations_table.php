038f63db2f668e2c2ee33ff2a6d1a35a53fd8377
kjICE <karl.jones@ice-comms.co.uk>
2022-02-28 20:18:03 +0000

    add locations migraton

---
+++ backend/database/migrations/2022_02_26_144959_create_locations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('city');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->boolean('wheelchairAccessible');
+            $table->string('country');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
