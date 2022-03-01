0b7d2795d84e64a72c453786a26b0d707e5392b7
kjICE <karl.jones@ice-comms.co.uk>
2022-03-01 21:12:16 +0000

    add create Booking functionality

--- backend/app/Http/Controllers/BookingApiController.php
+++ backend/app/Http/Controllers/BookingApiController.php
@@ -3,12 +3,107 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Illuminate\Validation\ValidationException;
+use Carbon\Carbon;
 use App\Models\{
-    Booking
+    Booking,
+    Student
 };
 
 class BookingApiController extends Controller
 {
+
+    /**
+     * Parses caught errors
+     *
+     * @return associated Array / String
+     */
+    private function handleError(\Throwable $error) {
+        $code = 400;
+
+        if ($error instanceof ValidationException) {
+            $message = $error->errors();
+        } else if ($error instanceof \Exception) {
+        //     // student ID error
+            $message = json_decode($error->getMessage());
+        } else {
+            $message = $error->getMessage();
+            $code = $error->getCode();
+        }
+
+        return [$message, $code];
+    }
+
+    /**
+     * Validates Booking request form
+     *
+     * @return associated Array of form fields
+     * @throws ValidationException;
+     */
+    private function validateBooking(Request $request) {
+        // throws ValidationException if not valid
+        $form = $request->validate([
+            "course_id" => "required|integer|exists:courses,id",
+            "location_id" => "required|integer|exists:locations,id",
+            "trainer_id" => "required|integer|exists:trainers,id",
+            "student_ids" => "required|json",
+            "start" => "required|date|after_or_equal:today",
+            "end" => "required|date|after_or_equal:start",
+            "certificate_needed" => "required|boolean",
+            "comments" => "string|nullable"
+        ]);
+
+        // additional validation for student ID list
+        $form["students"] = $this->validateStudentIds($request->student_ids);
+
+        // parse dates
+        $form["start"] = Carbon::create($form["start"]);
+        $form["end"] = Carbon::create($form["end"]);
+
+        return $form;
+    }
+
+    /**
+     * Validates student ID list
+     *
+     * @return Array of Student IDs
+     * @throws ValidationException;
+     */
+    private function validateStudentIds(String $studentIds) {
+        // throw error in a consistent way similar to ValidationException
+        $error = (object) ["student_ids" => []];
+
+        try {
+            $students = json_decode($studentIds);
+
+            if (!is_array($students)) {
+                throw new \Exception("student_ids is not a valid array");
+            }
+
+            if (!count($students)) {
+                throw new \Exception("student_ids is an empty array");
+            }
+
+            $students = array_unique($students);
+
+            foreach ($students as $id) {
+                if (!is_integer($id)) {
+                    throw new \Exception("Student ID not an integer: $id");
+                }
+                if (!Student::find($id)) {
+                    throw new \Exception("Invalid Student ID: $id");
+                }
+            }
+
+            return $students;
+        } catch (\Exception $err) {
+            $error->student_ids = $err->getMessage();
+            throw new \Exception(json_encode($error));
+        }
+    }
+
+    // ============================================================================================
+
     /**
      * Display a listing of the resource.
      *
@@ -30,7 +125,18 @@ class BookingApiController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $code = 201;
+        $output = null;
+
+        try {
+            $form = $this->validateBooking($request);
+
+            $output = Booking::create($form);
+        } catch (\Throwable $error) {
+            [$output["error"], $code] = $this->handleError($error);
+        }
+
+        return response($output, $code);
     }
 
     /**
