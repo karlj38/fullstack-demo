ec3661d8545b27a25ae4c5ae0346773eeec639e2
kjICE <karl.jones@ice-comms.co.uk>
2022-03-03 19:21:05 +0000

    update backend url

--- frontend/src/utils/fetchbackend.ts
+++ frontend/src/utils/fetchbackend.ts
@@ -1,6 +1,6 @@
 import qs from 'qs';
 
-const backendBaseUrl = 'http://localhost:5500'
+const backendBaseUrl = 'http://localhost/api'
 
 export async function getQuery(endpoint: string, params?: Record<string,any>) {
     return fetch(backendBaseUrl+'/'+endpoint+'?'+qs.stringify(params))
