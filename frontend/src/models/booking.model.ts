8a5699cd8759e6a93a59d4bc5219ef93f1187054
kjICE <karl.jones@ice-comms.co.uk>
2022-03-05 11:30:55 +0000

    add related resources to Booking TS model

--- frontend/src/models/booking.model.ts
+++ frontend/src/models/booking.model.ts
@@ -3,11 +3,14 @@ export interface Booking {
     course_id: number;
     location_id: number;
     trainer_id: number;
-    students: Array<number>;
+    students: Array<number | Object>;
     start: Date;
     end: Date;
     certificate_needed: Boolean;
     comments: String;
     created_at: Date;
     updated_at: Date;
+    course: Object;
+    location: Object;
+    trainer: Object;
 }
