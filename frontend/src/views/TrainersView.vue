7314ab9e3689dfa533f2ccd87d5dea6a89803b39
kjICE <karl.jones@ice-comms.co.uk>
2022-03-06 21:48:50 +0000

    add city filter to Trainers view

--- frontend/src/views/TrainersView.vue
+++ frontend/src/views/TrainersView.vue
@@ -1,5 +1,13 @@
 <template>
   <h1>List of Trainers</h1>
+  <v-row>
+    <v-col md="2">
+      <v-text-field
+        v-model="city"
+        label="Search by city"
+      />
+    </v-col>
+  </v-row>
   <v-container
       class="d-flex flex-row flex-wrap"
   >
@@ -8,7 +16,7 @@
       indeterminate
     />
     <TrainerCard
-      v-for="trainer of trainers"
+      v-for="trainer of filteredTrainers"
       :trainer="trainer"
       :show="true"
     />
@@ -19,7 +27,21 @@
 
 import TrainerCard from "../components/TrainerCard.vue";
 import {getQuery} from "../utils/fetchbackend";
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref} from "vue";
+
+const city = ref("")
+
+const filteredTrainers = computed(() => {
+  let output = [...trainers.value];
+
+  if (trainers.value.length && city.value) {
+    output = output.filter(trainer => {
+      return trainer.city.toLowerCase().startsWith(city.value.toLowerCase());
+    });
+  }
+
+  return output
+})
 
 const trainers = ref([])
 
